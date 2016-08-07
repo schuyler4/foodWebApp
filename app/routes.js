84301220414ee0fa5cc5773b9d71f07f4bddb676
Marek Newton <marek.s.newton@gmail.com>
2016-08-06 23:11:38 -0600

    first commit

---
+++ app/routes.js
@@ -0,0 +1,84 @@
+const User = require('../app/models/users')
+const Meal = require('../app/models/users')
+const loggedIn = require('../config/passport')
+
+module.exports = (app, passport) => {
+    
+    app.get('/', (req, res) => {
+        res.render('index')
+        if(isLoggedIn) {
+            console.log(user)
+        }
+        else {
+            console.log('you are not logged in')
+        }
+    });
+    
+    app.post('/addMeal', (req, res) => {
+        
+        if(isLoggedIn) {
+            let meal = new Meal({
+                title: req.body.title
+            })
+
+            meal.save((err) => {
+                if(err) {
+                    throw(err)
+                }
+                else {
+                    console.log('a meal has been saved sucsesfully')
+                }
+            })
+            //console.log(User)
+        }
+        else {
+            console.log('not logged in')
+            res.redirect('/login')
+        }
+        res.redirect('/')
+    })
+
+    app.get('/login', (req, res) => {
+        res.render('login', { message: req.flash('loginMessage') }); 
+    });
+    
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/graph',
+        failureRedirect : '/login', 
+        failureFlash : true 
+    }))
+
+    app.get('/signup', (req, res) => {
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+    
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/graph', 
+        failureRedirect : '/signup', 
+        failureFlash : true 
+    }));
+
+    app.get('/graph', isLoggedIn, (req, res) => {
+        if(isLoggedIn) {
+            res.render('graph', {
+                user : req.user 
+            });
+        }
+        else {
+            res.redirect('/')
+        }
+    });
+    
+    app.get('/logout', (req, res) => {
+        req.logout();
+        res.redirect('/')
+    });
+};
+
+function isLoggedIn(req, res, next) {
+
+    if (req.isAuthenticated())
+        return next()
+    
+    res.redirect('/')
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+
+const express  = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const configDB = require('./config/data.js');
+
+mongoose.connect(configDB.url); 
+
+require('./config/passport')(passport) 
+
+
+app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(bodyParser())
+
+app.set('view engine', 'ejs')
+
+app.use(session({ secret: 'this isent the real secret' }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
+require('./app/routes.js')(app, passport)
+
+app.listen(port);
+console.log('listening on port ' + port);
+
+
